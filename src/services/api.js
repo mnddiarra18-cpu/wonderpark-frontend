7350c94fe2c7b245dbcf63a414b4dce3891f2bd4
Baly Ndiaye <baly18@gmail.com>
2026-05-11 03:46:15 +0000

    fix API base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://wonderpark-backend.onrender.com/api/users";
+const API_URL = "https://wonderpark-backend.onrender.com/api";
 // Configuration axios
 const api = axios.create({
   baseURL: API_URL,
